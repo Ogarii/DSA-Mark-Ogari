533417328588069154ddec9d22f616c63b3bb70e
Ogarii <128757253+Ogarii@users.noreply.github.com>
2024-04-23 19:37:37 +0300

    QUIZ2.question3.cpp

--- QUIZ2.question3.cpp
+++ QUIZ2.question3.cpp
@@ -18,11 +18,9 @@ Node* reverseLinkedList(Node* head) {
         current = nextNode;
     }
 
-    // prev now points to the new head of the reversed list
     return prev;
 }
 
-// Function to print the linked list
 void printList(Node* head) {
     while (head != nullptr) {
         std::cout << head->value << " ";
@@ -32,20 +30,16 @@ void printList(Node* head) {
 }
 
 int main() {
-    // Example linked list:
     Node* head = new Node(1);
     head->next = new Node(4);
     head->next->next = new Node(6);
     head->next->next->next = new Node(7);
     head->next->next->next->next = new Node(12);
 
-    // Reverse the linked list
     Node* reversedHead = reverseLinkedList(head);
 
-    // Print the reversed linked list
     printList(reversedHead);
 
-    // Clean up memory
     while (reversedHead != nullptr) {
         Node* temp = reversedHead;
         reversedHead = reversedHead->next;
